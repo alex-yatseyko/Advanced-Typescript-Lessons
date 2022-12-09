49e504aea07b148c2424bc4a8288a7ca98d233f2
Alex <34627441+alex-yatseyko@users.noreply.github.com>
2022-12-09 02:13:04 +0200

    Lesson 1

--- lesson-1.ts
+++ lesson-1.ts
@@ -1,5 +1,16 @@
-function serializeJSON(value: any) {
-  return JSON.stringify(value);
+// Use the Optional Chaining Operator in TypeScript
+
+type SerializeOptions = {
+  formatting?: {
+    indent?: number;
+    getIndent?: () => number;
+  };
+};
+
+function serializeJSON(value: any, options?: SerializeOptions) {
+  const indent =
+    options?.formatting?.indent || options?.formatting?.getIndent?.();
+  return JSON.stringify(value, null, indent);
 }
 
 const user = {
@@ -7,6 +18,11 @@ const user = {
   age: 27,
 };
 
-const json = serializeJSON(user);
+const json = serializeJSON(user, {
+  formatting: {
+    // indent: 2,
+    getIndent: () => 2,
+  },
+});
 
 console.log(json);
