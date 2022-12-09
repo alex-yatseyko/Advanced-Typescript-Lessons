e9f2d0b34ba3216d389c79e3d99f344e3c62dd52
Alex <34627441+alex-yatseyko@users.noreply.github.com>
2022-12-09 17:29:52 +0200

    Lesson 7

--- lesson-6.ts
+++ lesson-6.ts
@@ -1,13 +1,16 @@
 class Counter {
   //   _value = 0; // This property is private. It's available only in class, but it's just a naming
-  private _value = 0;
+  //   private _value = 0;
+  #value = 0; // truly private value
 
   increment() {
-    this._value += 1;
+    // this._value += 1;
+    this.#value += 1;
   }
 
   get count() {
-    return this._value;
+    // return this._value;
+    return this.#value;
   }
 }
 
@@ -18,5 +21,7 @@ counter.increment();
 
 // counter._value = -100
 // counter["_value"].= 100 // Will break the private modifier
+
+// counter.#value = -100; // Is not accessible outside of class 'Counter'
 
 console.log(counter.count);
