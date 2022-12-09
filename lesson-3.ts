cb914af653ec72f8be24e6f2fa1165d5d0cbb5c9
Alex <34627441+alex-yatseyko@users.noreply.github.com>
2022-12-09 12:52:16 +0200

    Lesson 4

--- lesson-3.ts
+++ lesson-3.ts
@@ -1,4 +1,5 @@
 // Statically Type Unknown Values with TypeScript's unknown Type
+// Narrow the unknown Type with TypeScript's Assertion Functions
 
 let value: unknown;
 value = true;
@@ -14,11 +15,34 @@ if (typeof value === "string") {
   console.log(uppercasedText);
 }
 
+// function assert(condition: boolean, message: string): asserts condition {
+//   if (!condition) {
+//     throw Error(message);
+//   }
+// }
+
+function asserIsNumber(value: unknown, name: string): asserts value is number {
+  if (typeof value !== "number") {
+    throw Error(`Espected ${name} to be a number`);
+  }
+}
+
 function range(from: number, to: number): number[];
 function range(from: unknown, to: unknown): number[] {
-  if (typeof from !== "number" || typeof to !== "number") {
-    throw Error("range() expects exactly 2 numbers");
-  }
+  //   assert(
+  //     typeof from === "number" || typeof to === "number",
+  //     "range() expects exactly 2 numbers"
+  //   );
+
+  //   assert(typeof from === "number", "Form should be number");
+  //   assert(typeof to === "number", "To should be number");
+
+  asserIsNumber(from, "From");
+  asserIsNumber(to, "To");
+
+  //   if (typeof from !== "number" || typeof to !== "number") {
+  //     throw Error("range() expects exactly 2 numbers");
+  //   }
 
   const values: number[] = [];
   for (let i = from; i < to; i++) {
@@ -28,3 +52,5 @@ function range(from: unknown, to: unknown): number[] {
 }
 
 console.log(range(0, 5));
+// console.log(range(0, "T" as any));
+// console.log(range("F" as any, 5));
